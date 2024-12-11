98b1b9e47557508727fafdefb38eeb7da4526298
Nando Alvarado <nandoalvarado022@gmail.com>
2024-12-11 16:38:21 -0500

    instalando firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCw1GGM-vklHvWP_6YZa8redkFdm0cJXQE",
+  authDomain: "pikplay-72843.firebaseapp.com",
+  projectId: "pikplay-72843",
+  storageBucket: "pikplay-72843.firebasestorage.app",
+  messagingSenderId: "1095528210985",
+  appId: "1:1095528210985:web:39d7a6f1277a4451caf238",
+  measurementId: "G-5L0TTHX1CT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
