f8d9f2e9c2fe26efbf5cd07e00a95d0393cc633f
Nando Alvarado <nandoalvarado022@gmail.com>
2024-12-12 16:07:58 -0500

    files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,18 @@
-import * as React from 'react';
+import Layout from "../components/layout/Layout"
+import Onboarding from "../components/onboarding/Onboarding"
 
-export default function Index() {
-  return (
-    <div>Hi World!</div>
-  );
+const IndexPage = () => {
+    const image = ''
+    const descripcion = ''
+    const title = 'Te presentamos Pikplay Ally'
+    const url = ''
+    return <Layout
+        image={image}
+        descripcion={descripcion}
+        title={title}
+        url={url}>
+        <Onboarding />
+    </Layout>   
 }
+
+export default IndexPage;
